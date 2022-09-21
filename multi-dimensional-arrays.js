92301011101c437b860cd915b4fc49de87f14414
Letizia DI MAIO <109817389+leta91@users.noreply.github.com>
2022-09-21 08:50:22 +0100

    Add JS code

---
+++ multi-dimensional-arrays.js
@@ -0,0 +1,8 @@
+let myNestedArray = [
+  // Only change code below this line
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  ['loop', 'shift', 6, 7, 1000, ['deep'], 'method', true],
+  ['concat', false, true, 'spread', 'array', 24, [['deeper']]],
+  ['mutate', 1327.98, 'splice', 'slice', 'push', [[['deepest']]], false]
+  // Only change code above this line
+];
